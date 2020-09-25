8575b3fdb63fb7d677800ca6c0659c4477a66e2c
Semert <semert2121@gmail.com>
2020-09-25 18:55:50 +0300

    redux setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
 import App from "./App";
+import { reducer } from "./reducers";
+import { Provider } from "react-redux";
+
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+const INITIAL_STATE = {
+  yazıListesi: ["deneme"],
+};
+
+export const reducer = (state = INITIAL_STATE, action) => {
+  return state;
+};
